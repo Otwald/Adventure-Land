f26427a9412ce8f3ecec369c2ff88ea3222926b3
otwald <hinterlanddev@googlemail.com>
2019-05-05 09:26:04 +0200

    basic interaction test

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 // This is CODE, lets you control your character with code.
 // If you don't know how to code, don't worry, It's easy.
 // Just set attack_mode to true and ENGAGE!
-const base_url = 'https://github.com/Otwald/Adventure-Land/blob/master/main.js'
 const name = character.name;
 const party = ['Rukja', 'Alrinea', 'Umeko'];
+var name = require('./'+name);
 load_code("party_build");
 load_code("wrapper_action");
 load_code("chat_command");
@@ -21,19 +21,19 @@ main = (name, party) => {
 	switch (name) {
 		case 'Alrinea':
 			game_log('Alrinea loop2');
-			alrinea();
+			name.alrinea();
 			break;
 		case 'Otwald':
 			game_log('Otwald loop');
-			otwald(party);
+			name.otwald(party);
 			break;
 		case 'Umeko':
 			game_log('Umeko loop');
-			umeko(party);
+			name.umeko(party);
 			break;
 		case 'Rukja':
 			game_log('Rukja loop');
-			rukja();
+			name.rukja();
 			break;
 	}
 	performance_trick()
@@ -41,15 +41,3 @@ main = (name, party) => {
 } // Loops every 1/4 seconds.
 
 main(name, party);
-
-loadLink = (url) => {
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", url, true);
-	xhttp.onreadystatechange = function () {
-		if (this.readyState == 4 && this.status == 200) {
-			var script = this.response
-			eval.apply(window, [script])
-		}
-	};
-	xhttp.send();
-}
