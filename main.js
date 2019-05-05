ffb20d8649a02f98db89ed1fa5eaee0f5b62cfbf
otwald <hinterlanddev@googlemail.com>
2019-05-05 09:13:46 +0200

    initialize ajax

--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 // This is CODE, lets you control your character with code.
 // If you don't know how to code, don't worry, It's easy.
 // Just set attack_mode to true and ENGAGE!
+const base_url = 'https://github.com/Otwald/Adventure-Land/blob/master/main.js'
 const name = character.name;
-const party = ['Rukja','Alrinea','Umeko'];
+const party = ['Rukja', 'Alrinea', 'Umeko'];
 load_code("party_build");
 load_code("wrapper_action");
 load_code("chat_command");
@@ -12,12 +13,14 @@ load_code(3);
 load_code(4);
 load_code(5);
 
-main=(name,party)=>{
-	
+loadLink(base_url);
+
+main = (name, party) => {
+
 	//base_attack(true);
-	switch(name){
+	switch (name) {
 		case 'Alrinea':
-			game_log('Alrinea loop');
+			game_log('Alrinea loop2');
 			alrinea();
 			break;
 		case 'Otwald':
@@ -37,4 +40,16 @@ main=(name,party)=>{
 
 } // Loops every 1/4 seconds.
 
-main(name,party);
+main(name, party);
+
+loadLink = (url) => {
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", url, true);
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			var script = this.response
+			eval.apply(window, [script])
+		}
+	};
+	xhttp.send();
+}
