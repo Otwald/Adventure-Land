7b457cb5613d53f53a38d4e55127950837cda052
otwald <hinterlanddev@googlemail.com>
2019-10-26 09:45:35 +0200

    testing how to save requested scripts part 6

--- main.js
+++ main.js
@@ -11,28 +11,6 @@ required = (name) => {
 	});
 }
 
-const base_url = 'https://raw.githubusercontent.com/Otwald/Adventure-Land/master/'
-
-// var test;
-
-// loadLink = (url) => {
-// 	var xhttp = new XMLHttpRequest();
-// 	xhttp.open("GET", url, true);
-// 	xhttp.onreadystatechange = function () {
-// 		if (this.readyState == 4 && this.status == 200) {
-// 			var script = this.response;
-// 			test = script;
-// 			eval.apply(window, [script])
-// 		}
-// 	};
-// 	xhttp.send();
-// }
-// const name = window.character.name;
-
-// loadLink(base_url + name.toLowerCase() + '.js');
-
-// console.log(test);
-
 const party = ['Rukja', 'Alrinea', 'Umeko'];
 const alrinea = required('alrinea');
 // console.log(alrinea)
