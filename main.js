7556e44068fd051c2b6b5eacddab34f7c8f957ff
otwald <hinterlanddev@googlemail.com>
2019-10-31 10:54:56 +0100

    one step before giving up

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 async function main(baseUrl) {
-	async function require(name) {
-		const result = await $.ajax(baseUrl + name + '.js', {
-			type: 'Get',
-			dataType: "script",
-			cache: true,
-		});
-		console.log(result);
-		return result;
-	}
+	// async function require(name) {
+	// 	const result = await $.ajax(baseUrl + name + '.js', {
+	// 		type: 'Get',
+	// 		dataType: "text",
+	// 		cache: true,
+	// 	});
+	// 	console.log(result);
+	// 	return result;
+	// }
 
+
+	
 	let name = window.character.id;
-	await require('basecharacter');
-	let res = await require(name.toLowerCase());
-	console.log(res);
+	var imported = document.createElement('script');
+	imported.src = baseUrl + name.toLowerCase() + '.js';
+	document.head.appendChild(imported);
+	// await require('basecharacter');
+	// let res = await require(name.toLowerCase());
+	// console.log(res);
 	let person = new window[name];
 	person.handle();
 	
